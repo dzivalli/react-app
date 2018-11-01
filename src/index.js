a59bb945881089f07d8f3840778d902bc022b163
stan <stan@onemob.com>
2018-11-01 12:46:31 -0700

    add my-math module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log('hello world');
+import MyMath from './my-math';
+
+console.log(MyMath.calculateSum(1, 2, 3, 4, 5));
---
+++ src/my-math.js
@@ -0,0 +1,11 @@
+export default class MyMath {
+  static calculateSum(...args) {
+    let sum = 0;
+
+    for (let i=0; i<args.length; i++) {
+      sum += args[i];
+    }
+
+    return sum;
+  }
+}
