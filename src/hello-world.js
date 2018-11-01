57c5ce2ab5ca2b4580d73f43e0048482e5f3bdbc
stan <stan@onemob.com>
2018-11-01 16:55:43 -0700

    add recipient class

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Receipt from './receipt';
 
 class HelloWorld extends React.Component {
   render() {
+    let receipt = new Receipt();
+    receipt.addItem('potato', 10);
+    receipt.addItem('tomato', 20);
+
     return (
       <div>
-        Hello world!!!
+        <ul>
+          {
+            receipt.getItems().map((item) => (
+              <li>
+                <strong>Name:</strong>  {item.name}, <strong>Amount:</strong> {item.amount}
+              </li>
+            ))
+          }
+        </ul>
+        <div>Total: {receipt.getTotal()}</div>
       </div>
     );
   }
