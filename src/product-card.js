4c8985eb19cda87466568ac827c3795923b77216
stan <stan@onemob.com>
2018-11-09 16:28:53 -0800

    add product-card with depended components

---
+++ src/product-card.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+import Price from './price';
+import TextBox from './text-box';
+import Image from './image';
+
+class ProductCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const product = this.props.product;
+    return (
+      <div>
+        <Image src={product.imageUrl}
+               alt={product.title}
+               width={100}
+               height={50}>
+        </Image>
+        <TextBox text={product.title}></TextBox>
+        <Price price={product.price}></Price>
+      </div>
+    )
+  }
+}
+
+export default ProductCard;
---
+++ src/text-box.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TextBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <span>
+        {this.props.text}
+      </span>
+    )
+  }
+}
+
+export default TextBox;
