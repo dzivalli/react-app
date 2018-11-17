69f472e21284e2f55c01bee1643f0114a6344fd3
stan <stan@onemob.com>
2018-11-17 13:21:38 -0800

    add cart-context to catalog-page

--- src/catalog-page/catalog-page.js
+++ src/catalog-page/catalog-page.js
@@ -1,22 +1,42 @@
 import React, { Component } from "react";
 
 import Catalog from "./catalog";
+import CartContext from "./cart-context";
+import CartButton from "./cart-button";
 
 class CatalogPage extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      productIds: []
+    };
+
+    this.addProduct = this.addProduct.bind(this);
+  }
+
+  addProduct(productId) {
+    let productIds = this.state.productIds.slice(0);
+    productIds.push(productId);
+
+    this.setState({ productIds });
   }
 
   render() {
     return (
-      <div className="container">
-        <div className="row my-3">
-          <div className="col">
-            <button className="btn btn-primary float-right">Checkout</button>
+      <CartContext.Provider value={{
+        productIds: this.state.productIds,
+        addProduct: this.addProduct
+      }}>
+        <div className="container">
+          <div className="row my-3">
+            <div className="col">
+              <CartButton />
+            </div>
           </div>
+          <Catalog products={this.props.products}></Catalog>
         </div>
-        <Catalog products={this.props.products}></Catalog>
-      </div>
+      </CartContext.Provider>
     )
   }
 }
