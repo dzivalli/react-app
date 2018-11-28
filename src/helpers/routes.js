09bc9dddc0f77777120cf291439d025672d516da
stan <stan@onemob.com>
2018-11-28 14:36:48 -0800

    add paths helper

---
+++ src/helpers/routes.js
@@ -0,0 +1,4 @@
+export const productsPath = () => "/";
+export const productPath = (id = ":id") => `/products/${id}`;
+export const cartPath = () => "/cart";
+export const contactsPath = () => "/contacts";
